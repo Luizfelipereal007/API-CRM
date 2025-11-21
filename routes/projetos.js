75b26d942b9d0c5c008825c7b49485dc454b24c8
lbonfim <lbonfim@grupoimagetech.com.br>
2025-11-21 12:02:22 -0400

    feat(api): :dizzy: add project search routes by email, name, and CPF

--- routes/projetos.js
+++ routes/projetos.js
@@ -9,6 +9,12 @@ router.get('/:id', ProjetoController.buscarProjeto);
 
 router.get('/usuario/:usuarioId', ProjetoController.listarProjetosPorUsuario);
 
+router.get('/email/:email', ProjetoController.buscarProjetosPorEmail);
+
+router.get('/nome/:nome', ProjetoController.buscarProjetosPorNome);
+
+router.get('/cpf/:cpf', ProjetoController.buscarProjetosPorCpf);
+
 router.post('/', ProjetoController.criarProjeto);
 
 router.put('/:id', ProjetoController.atualizarProjeto);
