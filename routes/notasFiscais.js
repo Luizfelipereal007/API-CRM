6c19b248ef9aff9d3db851027908b861ee96a717
lbonfim <lbonfim@grupoimagetech.com.br>
2025-11-21 12:16:30 -0400

    feat(api): :dizzy: add email, name, CPF search endpoints for fiscal notes

--- routes/notasFiscais.js
+++ routes/notasFiscais.js
@@ -9,6 +9,12 @@ router.get('/projeto/:projetoId', NotaFiscalController.listarNotasFiscaisPorProj
 
 router.get('/usuario/:usuarioId', NotaFiscalController.listarNotasFiscaisPorUsuario);
 
+router.get('/email/:email', NotaFiscalController.buscarNotasFiscaisPorEmail);
+
+router.get('/nome/:nome', NotaFiscalController.buscarNotasFiscaisPorNome);
+
+router.get('/cpf/:cpf', NotaFiscalController.buscarNotasFiscaisPorCpf);
+
 router.delete('/:id', NotaFiscalController.deletarNotaFiscal);
 
 export default router;
