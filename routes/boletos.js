6d5171ab7a987196f4f906dbfd703ad5cd7a9a21
lbonfim <lbonfim@grupoimagetech.com.br>
2025-11-21 12:08:59 -0400

    feat(api): :dizzy: add boleto search routes by email, name, and CPF

--- routes/boletos.js
+++ routes/boletos.js
@@ -9,6 +9,12 @@ router.get('/projeto/:projetoId', BoletoController.listarBoletosPorProjeto);
 
 router.get('/usuario/:usuarioId', BoletoController.listarBoletosPorUsuario);
 
+router.get('/email/:email', BoletoController.buscarBoletosPorEmail);
+
+router.get('/nome/:nome', BoletoController.buscarBoletosPorNome);
+
+router.get('/cpf/:cpf', BoletoController.buscarBoletosPorCpf);
+
 router.post('/', BoletoController.criarBoleto);
 
 router.put('/:id/pagar', BoletoController.pagarBoleto);
