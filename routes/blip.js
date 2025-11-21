cc8f64230965e6a3c415f64f8ac42d4f4c294b53
lbonfim <lbonfim@grupoimagetech.com.br>
2025-11-21 13:42:55 -0400

    refactor(api): :fire: remove blip route endpoint

--- routes/blip.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import BlipController from '../controller/BlipController.js';
-
-const router = express.Router();
-
-router.get('/getResumoPagamentosPorCpf', BlipController.getResumoPagamentosPorCpf);
-
-export default router;
-
