f462fd1c8183a51295649abc7a7d28f3d06053da
lbonfim <lbonfim@grupoimagetech.com.br>
2025-11-21 13:28:51 -0400

    refactor(api): :sparkles: remove unused search endpoints

--- routes/blip.js
+++ routes/blip.js
@@ -3,12 +3,6 @@ import BlipController from '../controller/BlipController.js';
 
 const router = express.Router();
 
-router.get('/getProjetosPorEmail', BlipController.getProjetosPorEmail);
-
-router.get('/getBoletosPorCpf', BlipController.getBoletosPorCpf);
-
-router.get('/getNotasFiscaisPorCpf', BlipController.getNotasFiscaisPorCpf);
-
 router.get('/getResumoPagamentosPorCpf', BlipController.getResumoPagamentosPorCpf);
 
 export default router;
